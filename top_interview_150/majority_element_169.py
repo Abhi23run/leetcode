f62f8abec98a9161311bb606fede2f1bb1d0e89d
Abhinav Arun <abhinavarun23@gmail.com>
2024-01-03 16:06:55 -0500

    Update majority_element_169.py

--- top_interview_150/majority_element_169.py
+++ top_interview_150/majority_element_169.py
@@ -1,8 +1,23 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+
+        ##Time Complexity: O(n), Space Complexity: O(n)
         n=len(nums)
         freq_dict={}
         for num in nums:
             freq_dict[num]=freq_dict.get(num,0)+1
 
         return [i for i,j in freq_dict.items() if j>n//2][0]
+
+
+### Improved Solution as existence of a majority solutions is guaranteed
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        ##Time Complexity: O(n), Space Complexity: O(1) 
+        n=len(nums)
+        freq_dict={}
+        for num in nums:
+            freq_dict[num]=freq_dict.get(num,0)+1
+            if freq_dict[num]>n//2:
+                return num 
