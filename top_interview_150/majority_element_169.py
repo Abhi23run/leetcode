cce41d865d645bf7d966e0071baf509384e8402a
Abhinav Arun <abhinavarun23@gmail.com>
2024-01-03 16:09:08 -0500

    Update majority_element_169.py

--- top_interview_150/majority_element_169.py
+++ top_interview_150/majority_element_169.py
@@ -21,3 +21,7 @@ class Solution:
             freq_dict[num]=freq_dict.get(num,0)+1
             if freq_dict[num]>n//2:
                 return num 
+
+
+### Interesting Approach: Sorting 
+## The intuition behind this approach is that if an element occurs more than n/2 times in the array (where n is the size of the array), it will always occupy the middle position when the array is sorted. Therefore, we can sort the array and return the element at index n/2.
