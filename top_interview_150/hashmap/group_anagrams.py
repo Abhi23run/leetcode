cbe8a2fad4be92fd2ee0aa618a504362ec4a1bfb
Abhinav Arun <abhinavarun23@gmail.com>
2025-01-15 22:18:05 -0500

    optimized solution for group anagrams - amazing solution

--- top_interview_150/hashmap/group_anagrams.py
+++ top_interview_150/hashmap/group_anagrams.py
@@ -1,31 +1,16 @@
 class Solution:
-    def isAnagram(self,s:str,t:str):
-        freq_dict_s={}
-        freq_dict_t={}
-
-        for char in s:
-            freq_dict_s[char]=freq_dict_s.get(char,0)+1
-        for char in t:
-            freq_dict_t[char]=freq_dict_t.get(char,0)+1
-
-        return freq_dict_s==freq_dict_t
-
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        n=len(strs)
-        unique_set=set()
-        output_list=[]
-        for i in range(n):
-            if strs[i] in unique_set:
-                continue
-            else:
-                unique_set.add(strs[i])
-                sublist=[strs[i]]
-                j=i+1
-                while(j<n):
-                    if self.isAnagram(strs[i],strs[j]):
-                        sublist.append(strs[j])
-                        unique_set.add(strs[j])
-                    j+=1
-                output_list.append(sublist)
+        result=defaultdict(list)
 
-        return output_list
+        for s in strs:
+            count=[0]*26
+            for c in s:
+                count[ord(c)-ord("a")]+=1
+            
+            result[tuple(count)].append(s)
+
+        return list(result.values())
+
+
+
+        
