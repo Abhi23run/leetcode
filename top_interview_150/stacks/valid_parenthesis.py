80fae79fe8653fe5deda8ba731dc01c8c0c186d8
Abhinav Arun <abhinavarun23@gmail.com>
2025-01-19 13:06:46 -0500

    stack problem

---
+++ top_interview_150/stacks/valid_parenthesis.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        close_to_open_map={')':'(','}':'{',']':'['}
+        stack=[]
+
+        for char in s:
+            if char in close_to_open_map:
+                if stack and stack[-1]== close_to_open_map[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char) 
+                
+        return True if len(stack)==0 else False
